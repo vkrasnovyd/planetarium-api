0a577ab577672bd686270a932725e274a891f775
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-28 01:14:19 +0100

    Add 'validate_seat' method to 'Ticket' model

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 from django.db import models
+from rest_framework.exceptions import ValidationError as RestValidationError
+
 from planetarium_api import settings
 
 
@@ -115,7 +117,48 @@ class Ticket(models.Model):
     seat = models.PositiveSmallIntegerField()
 
     class Meta:
-        ordering = ["reservation", "show_session"]
+        ordering = ["reservation", "show_session", "row", "seat"]
+        unique_together = ["show_session", "row", "seat"]
 
     def __str__(self):
         return f"{self.show_session} - (row: {self.row}, seat: {self.seat})"
+
+    @staticmethod
+    def validate_seat(
+        row_number: int,
+        seat: int,
+        planetarium_dome: PlanetariumDome,
+        error_to_raise,
+    ):
+        row = SeatRow.objects.get(
+            planetarium_dome=planetarium_dome, row_number=row_number
+        )
+        if not (1 <= seat <= row.seats_in_row):
+            raise error_to_raise(
+                {
+                    "seat": (
+                        f"seat must be in range [1, {row.seats_in_row}], "
+                        f"not {seat}"
+                    )
+                }
+            )
+
+    def clean(self):
+        Ticket.validate_seat(
+            row_number=self.row,
+            seat=self.seat,
+            planetarium_dome=self.show_session.planetarium_dome,
+            error_to_raise=RestValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
