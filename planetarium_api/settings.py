9e46b294a4f3646b0effcc73d55a83be8d762e11
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-29 22:24:40 +0100

    Generate basic documentation for the project

--- planetarium_api/settings.py
+++ planetarium_api/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "rest_framework",
     "debug_toolbar",
+    "drf_spectacular",
     "planetarium",
     "user",
 ]
@@ -138,3 +139,20 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Bus station API",
+    "DESCRIPTION": "Order tickets for your trips",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelExpandDepth": 2,
+        "persistAuthorization": True,
+    },
+}
--- planetarium_api/urls.py
+++ planetarium_api/urls.py
@@ -18,6 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +32,15 @@ urlpatterns = [
         include("planetarium.urls", namespace="planetarium"),
     ),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
