cec64889a42a3db57fd1e2c42189fede826e0801
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-29 18:04:10 +0100

    Implement token authentication

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", CreateTokenView.as_view(), name="token"),
 ]
 
 app_name = "user"
