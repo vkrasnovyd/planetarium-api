dc6ad5b378747a21da562f9c06b1941645241cd8
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-29 21:28:58 +0100

    Set up media files serving

--- planetarium_api/urls.py
+++ planetarium_api/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -25,4 +27,4 @@ urlpatterns = [
         include("planetarium.urls", namespace="planetarium"),
     ),
     path("api/user/", include("user.urls", namespace="user")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
